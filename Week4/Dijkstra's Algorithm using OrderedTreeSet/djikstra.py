3706660e3a4db8ab78c86a0d9de1c01e56d90b42
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-09-28 21:30:04 -0500

    Djikstra graphics using OTS

---
+++ Week4/Dijkstra's Algorithm using OrderedTreeSet/djikstra.py
@@ -0,0 +1,187 @@
+import sys
+import turtle
+from xml.dom import minidom
+from orderedtreeset import OrderedTreeSet
+
+class Vertex:
+    def __init__(self,vertexId,x,y,label):
+        self.vertexId = vertexId
+        self.x = x
+        self.y = y
+        self.label = label
+        self.adjacents = []
+        self.previous = None
+
+    def getAdjacents(self, edgeList):
+        for e in edgeList:
+            if e.v1 == int(self.vertexId):
+                self.adjacents.append(e)
+            elif e.v2 == int(self.vertexId):
+                self.adjacents.append(e)
+        return self.adjacents
+        
+class Edge:
+    def __init__(self,v1,v2,weight=0):
+        self.v1 = v1
+        self.v2 = v2
+        self.weight = weight
+    
+    def getVertices(self):
+        return (self.v1, self.v2)
+
+    def __lt__(self, other):
+        if type(self) != type(other):
+            raise Exception("Unorderable types")
+        return self.weight < other.weight
+
+class Pair(object):
+    def __init__(self, vertexId, cost=0):
+        self.vertexId = vertexId
+        self.cost = cost
+    
+    def __lt__(self, other):
+        if type(self) != type(other):
+            raise Exception("Unorderable types")
+        return self.cost < other.cost
+
+
+def main():
+    xmldoc = minidom.parse("graph.xml")
+    
+    graph = xmldoc.getElementsByTagName("Graph")[0]
+    vertices = graph.getElementsByTagName("Vertices")[0].getElementsByTagName("Vertex")
+    edges = graph.getElementsByTagName("Edges")[0].getElementsByTagName("Edge")
+    
+    width = float(graph.attributes["width"].value)
+    height = float(graph.attributes["height"].value)
+
+    t = turtle.Turtle()
+    screen = t.getscreen()
+    screen.setworldcoordinates(0,height,width,0)
+    t.speed(1000)
+    t.ht()
+
+    # mapping vertexIds to objects
+    vertexDict = {}
+    for vertex in vertices:
+        vertexId = int(vertex.attributes["vertexId"].value)
+        x = float(vertex.attributes["x"].value)
+        y = float(vertex.attributes["y"].value)
+        label = vertex.attributes["label"].value
+        if label == "0":
+            sourceId = vertexId
+        v = Vertex(vertexId, x, y, label)
+        vertexDict[vertexId] = v
+
+    sourcePair = Pair(sourceId, 0)
+    # Creating a visited and unvisited set.
+    visited = set()
+    unvisited = OrderedTreeSet([sourcePair])
+    # Building list of edges.
+    edgeList = []
+    
+    for edge in edges:
+        anEdge = Edge(int(edge.attributes["head"].value), int(edge.attributes["tail"].value))
+        if "weight" in edge.attributes:       
+            anEdge.weight = float(edge.attributes["weight"].value) 
+        edgeList.append(anEdge)
+
+    # build labels
+    labelsDict = {int(v.attributes["label"].value):int(v.attributes["vertexId"].value) for v in vertices}
+    labels = [labelsDict[i] for i in range(len(labelsDict))]
+
+    # build list of distances from sourceId to other vertexIds
+    distances = [sys.maxsize for x in range(len(vertices))]
+    distances[sourceId] = 0
+
+    # building list of previous
+    previous = [-1 for x in range(len(vertices))]
+    previous[sourceId] = sourceId
+    previousLabelsDict = {v: k for k, v in labelsDict.items()}
+
+    # get adjacents of sourceId
+    while len(unvisited) != 0:
+        current = unvisited.tree.getSmallest()
+        unvisited.remove(current.getVal())
+
+        visited.add(current.getValId())
+
+        currentVertex = vertexDict[current.getValId()]
+        # grab adjacents.
+        adjacents = currentVertex.getAdjacents(edgeList)
+
+        for e in adjacents:
+            if e.v1 not in visited:
+                if distances[e.v1] > (distances[currentVertex.vertexId] + e.weight):
+                    distances[e.v1] = distances[currentVertex.vertexId] + e.weight
+                    previous[e.v1] = currentVertex.vertexId
+                    unvisited.add(Pair(e.v1, distances[currentVertex.vertexId] + e.weight))
+            if e.v2 not in visited:
+                if distances[e.v2] > (distances[currentVertex.vertexId] + e.weight):
+                    distances[e.v2] = distances[currentVertex.vertexId] + e.weight
+                    previous[e.v2] = currentVertex.vertexId
+                    unvisited.add(Pair(e.v2, distances[currentVertex.vertexId] + e.weight))
+
+
+    for i in range(len(visited)):
+        print("Vertex:")
+        print("  label: {}".format(i))
+        print("  cost: {:.2f}".format(distances[labels[i]]))
+        print("  previous:{}\n".format(previousLabelsDict[previous[labelsDict[i]]])) 
+
+    print(previous)
+
+    for edge in edgeList:
+        x1 = float(vertexDict[edge.v1].x)
+        y1 = float(vertexDict[edge.v1].y)
+        x2 = float(vertexDict[edge.v2].x)
+        y2 = float(vertexDict[edge.v2].y)
+        t.penup()
+        t.goto(x1,y1)
+        t.pendown()
+        t.goto(x2,y2)
+        if edge.weight != 0:       
+            x = (x1 + x2) / 2
+            y = (y1 + y2) / 2
+            t.penup()
+            t.goto(x,y)
+            t.write(str(edge.weight),align="center",font=("Arial",12,"normal"))
+                      
+
+    
+    for vertexId in vertexDict:
+        t.color("black")
+        vertex = vertexDict[vertexId]
+        x = vertex.x
+        y = vertex.y
+        t.penup()
+        t.goto(x,y-20)
+        
+        t.pendown()
+        t.fillcolor(0.8,1,0.4)
+        t.begin_fill()
+        t.circle(20)
+        t.end_fill()
+        t.penup()
+        t.goto(x+2,y+11)
+        t.write(vertex.label,align="center",font=("Arial",12,"bold"))
+
+        current = vertexId
+        while current != 15:
+            c = int(current)
+            x1 = float(vertexDict[c].x)
+            y1 = float(vertexDict[c].y)
+            current = previous[current]
+            x2 = float(vertexDict[current].x)
+            y2 = float(vertexDict[current].y)
+            t.color("red")
+            t.penup()
+            t.goto(x1,y1)
+            t.pendown()
+            t.goto(x2,y2)
+    
+    screen.exitonclick()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Week4/Dijkstra's Algorithm using OrderedTreeSet/orderedtreeset.py
@@ -0,0 +1,339 @@
+import random
+
+class OrderedTreeSet:
+    class BinarySearchTree:
+        # This is a Node class that is internal to the BinarySearchTree class. 
+        class Node:
+            def __init__(self,val,left=None,right=None):
+                self.val = val
+                self.left = left
+                self.right = right
+
+            def getVal(self):
+                return self.val
+
+            def getValCost(self):
+                return self.val.cost
+
+            def getValId(self):
+                return self.val.vertexId
+            
+            def setVal(self,newval):
+                self.val = newval
+                
+            def getLeft(self):
+                return self.left
+            
+            def getRight(self):
+                return self.right
+            
+            def setLeft(self,newleft):
+                self.left = newleft
+                
+            def setRight(self,newright):
+                self.right = newright
+                
+            # This method deserves a little explanation. It does an inorder traversal
+            # of the nodes of the tree yielding all the values. In this way, we get
+            # the values in ascending order.
+            # def __iter__(self):
+            #     if self.left != None:
+            #         for elem in self.left:
+            #             yield elem
+                        
+            #     yield self.val
+                
+            #     if self.right != None:
+            #         for elem in self.right:
+            #             yield elem
+
+            def __repr__(self):
+                return "BinarySearchTree.Node(" + repr(self.val) + "," + repr(self.left) + "," + repr(self.right) + ")"            
+                
+        # Below are the methods of the BinarySearchTree class. 
+        def __init__(self, root=None):
+            self.root = root
+            
+        def insert(self,val):
+            self.root = OrderedTreeSet.BinarySearchTree.__insert(self.root,val)
+            
+        def __insert(root,val):
+            if root == None:
+                return OrderedTreeSet.BinarySearchTree.Node(val)
+            
+            if val < root.getVal():
+                root.setLeft(OrderedTreeSet.BinarySearchTree.__insert(root.getLeft(),val))
+            else:
+                root.setRight(OrderedTreeSet.BinarySearchTree.__insert(root.getRight(),val))
+                
+            return root
+        
+        def getLeftMost(node):
+            if node.getLeft() == None:
+                return node
+            return OrderedTreeSet.BinarySearchTree.getLeftMost(node.getLeft())
+        
+        def __getSmallest(node):
+            if node == None:
+                return None
+            if node.getLeft() == None:
+                return node
+            if node.getLeft():
+                return OrderedTreeSet.BinarySearchTree.__getSmallest(node.getLeft())
+            
+
+        def getSmallest(self):
+            rv = OrderedTreeSet.BinarySearchTree.__getSmallest(self.root)
+            return rv
+
+        def delete(self, val):
+            self.root = OrderedTreeSet.BinarySearchTree.__delete(self.root, val)
+
+        def __delete(root, val):
+            if root == None:
+                return None
+
+            if val < root.getVal():
+                root.setLeft(OrderedTreeSet.BinarySearchTree.__delete(root.getLeft(),val))
+            elif val > root.getVal():
+                root.setRight(OrderedTreeSet.BinarySearchTree.__delete(root.getRight(),val))
+
+            elif val == root.getVal():
+                if root.getLeft() == None:
+                    return root.getRight()
+                elif root.getRight() == None:
+                    return root.getLeft()    
+                elif root.getLeft() and root.getRight():
+                    sub = OrderedTreeSet.BinarySearchTree.getLeftMost(root.getRight())
+                    # Current node val = leftmost child's val
+                    root.setVal(sub.getVal())
+                    # Delete left most child.
+                    root.setRight(OrderedTreeSet.BinarySearchTree.__delete(root.getRight(), sub.getVal()))
+
+            return root
+            
+            
+        def __iter__(self):
+            nodeStack = []
+            root = self.root
+            
+            # Push all left most nodes into the tree.
+            while root.getLeft() != None:
+                nodeStack.append(root)
+                root = root.getLeft()
+            nodeStack.append(root)
+
+            # Depth first search on the current node.
+            while len(nodeStack) > 0:
+                top = nodeStack.pop()
+                yield top.getVal()
+                rightSub = top.getRight()
+                if rightSub != None:
+                    nodeStack.append(rightSub)
+                    while rightSub.getLeft() != None:
+                        rightSub = rightSub.getLeft()
+                        nodeStack.append(rightSub)
+
+        def __find(node, item):
+            if node == None:
+                return node
+            
+            if node.getVal() == item:
+                return True
+            elif node.getVal() > item:
+                return OrderedTreeSet.BinarySearchTree.__find(node.getLeft(), item)
+            return OrderedTreeSet.BinarySearchTree.__find(node.getRight(), item)
+
+        def __contains__(self, item):
+            return OrderedTreeSet.BinarySearchTree.__find(self.root, item)
+
+        def __str__(self):
+            return "BinarySearchTree(" + repr(self.root) + ")"
+            
+    def __init__(self,contents=None):
+        self.tree = OrderedTreeSet.BinarySearchTree()
+        if contents != None:
+            # randomize the list
+            indices = list(range(len(contents)))
+            random.shuffle(indices)
+            
+            for i in range(len(contents)):
+                self.tree.insert(contents[indices[i]])
+                
+            self.numItems = len(contents)
+        else:
+            self.numItems = 0
+            
+    def __str__(self):
+        pass
+
+    def __iter__(self):
+        return iter(self.tree)
+    
+    # Following are the mutator set methods 
+    def add(self, item):
+        self.tree.insert(item)
+        self.numItems += 1
+                
+    def remove(self, item):
+        if item in self:
+            self.tree.delete(item)
+            self.numItems -= 1
+            return
+        raise KeyError("Value not found.")
+        
+    def discard(self, item):
+        self.tree.delete(item)
+        self.numItems -= 1
+        
+    def pop(self):
+        pass
+            
+    def clear(self):
+        self.tree = OrderedTreeSet.BinarySearchTree()
+        self.numItems = 0
+        
+    def update(self, other):
+        for item in other:
+            if item not in self:
+                self.add(item)
+            
+    def intersection_update(self, other):
+        # Review.
+        contents = list(self)
+
+        for item in contents:
+            if item not in other:
+                self.discard(item)
+            
+    def difference_update(self, other):
+        # Update the set, removing elements found in others.
+        contents = list(self)
+        for item in contents:
+            if item in other:
+                self.discard(item)
+                
+    def symmetric_difference_update(self, other):
+        pass
+                
+    # Following are the accessor methods for the HashSet  
+    def __len__(self):
+        return self.numItems
+
+    def __contains__(self, item):
+        if item in self.tree:
+            return True
+        return False
+
+        # OR
+        # Binary search O(log n)
+        # found = False
+        # if root:
+        #     while not found and root != None:
+        #         if item == root.getVal():
+        #             found = True
+        #         elif item > root.getVal():
+        #             root = root.getRight()
+        #         elif item < root.getVal():
+        #             root = root.getLeft()
+        # return found
+            
+    
+    # One extra method for use with the HashMap class. This method is not needed in the 
+    # HashSet implementation, but it is used by the HashMap implementation. 
+    def __getitem__(self, item):
+        pass      
+        
+    def not__contains__(self, item):
+        pass
+    
+    def isdisjoint(self, other):
+        pass
+    
+    
+    def issubset(self, other):
+        for item in self:
+            if item not in other:
+                return False
+        return True
+            
+    
+    def issuperset(self, other):
+        for item in other:
+            if item not in self:
+                return False
+        return True
+    
+    def union(self, other):
+        pass
+    
+    def intersection(self, other):
+        pass
+    #done
+    def difference(self, other):
+        contents = list(self)
+        new_set = OrderedTreeSet(contents)
+        new_set.difference_update(other)
+        
+        return new_set
+    
+    def symmetric_difference(self, other):
+        pass
+    
+    def copy(self):
+        return OrderedTreeSet(list(self))
+    
+    # Operator Definitions
+    def __or__(self, other):
+        pass
+    
+    def __and__(self,other):
+        pass
+    
+    def __sub__(self,other):
+        pass
+    
+    def __xor__(self,other):
+        pass
+    
+    def __ior__(self,other):
+        pass
+    
+    def __iand__(self,other):
+        pass
+    
+    def __ixor(self,other):
+        pass    
+    
+    def __le__(self,other):
+        pass
+    
+    def __lt__(self,other):
+        pass
+    
+    def __ge__(self,other):
+        pass
+    
+    def __gt__(self,other):
+        pass
+    
+    def __eq__(self,other):
+        return self.issubset(other) and other.issubset(self)     
+                
+            
+    
+ 
+def main():
+    s = input("Enter a list of numbers: ")
+    lst = s.split()
+    
+    tree = OrderedTreeSet()
+    
+    for x in lst:
+        tree.add(float(x))
+        
+    for x in tree:
+        print(x)
+
+if __name__ == "__main__":
+    main()
